8ffe894a714bdb95f710988194bf71a77e24566c
Sagar Karki <sagarkarki995@gmail.com>
2024-08-24 10:44:50 +0545

    1Oc to 10h exercises

--- scripts/10-exercises.js
+++ scripts/10-exercises.js
@@ -1,6 +1,7 @@
 const buttonElement = document.querySelector(".js-button");
 // console.log(buttonElement.classList.contains("js-button"));
 
+// 10d & 10e functions
 function gamingButtonOnOff() {
   const gamingButtonElement = document.querySelector(".gaming-button");
 
@@ -31,6 +32,55 @@ function techButtonOnOff() {
   }
 }
 
+// 10g functions
+function gamingOneButtonOnOff() {
+  const gamingButtonElement = document.querySelector(".js-gaming-button");
+  const musicButtonElement = document.querySelector(".js-music-button");
+  const techButtonElement = document.querySelector(".js-tech-button");
+
+  if (gamingButtonElement.classList.contains("is-toggled")) {
+    gamingButtonElement.classList.remove("is-toggled");
+    techButtonElement.classList.remove("is-toggled");
+    musicButtonElement.classList.remove("is-toggled");
+  } else {
+    gamingButtonElement.classList.add("is-toggled");
+    techButtonElement.classList.remove("is-toggled");
+    musicButtonElement.classList.remove("is-toggled");
+  }
+}
+
+function musicOneButtonOnOff() {
+  const gamingButtonElement = document.querySelector(".js-gaming-button");
+  const musicButtonElement = document.querySelector(".js-music-button");
+  const techButtonElement = document.querySelector(".js-tech-button");
+
+  if (musicButtonElement.classList.contains("is-toggled")) {
+    gamingButtonElement.classList.remove("is-toggled");
+    techButtonElement.classList.remove("is-toggled");
+    musicButtonElement.classList.remove("is-toggled");
+  } else {
+    musicButtonElement.classList.add("is-toggled");
+    techButtonElement.classList.remove("is-toggled");
+    gamingButtonElement.classList.remove("is-toggled");
+  }
+}
+
+function techOneButtonOnOff() {
+  const gamingButtonElement = document.querySelector(".js-gaming-button");
+  const musicButtonElement = document.querySelector(".js-music-button");
+  const techButtonElement = document.querySelector(".js-tech-button");
+
+  if (techButtonElement.classList.contains("is-toggled")) {
+    gamingButtonElement.classList.remove("is-toggled");
+    techButtonElement.classList.remove("is-toggled");
+    musicButtonElement.classList.remove("is-toggled");
+  } else {
+    techButtonElement.classList.add("is-toggled");
+    gamingButtonElement.classList.remove("is-toggled");
+    musicButtonElement.classList.remove("is-toggled");
+  }
+}
+
 function testButton() {
   const buttonElement = document.querySelector(".js-button");
   const testElement = document.querySelector(".js-test");
@@ -40,5 +90,20 @@ function testButton() {
   } else {
     buttonElement.classList.add("button");
     testElement.innerHTML = "";
+  }
+}
+
+function calculateAmazonCost() {
+  const inputCostElement = document.querySelector(".js-input");
+  let cost = Number(inputCostElement.value);
+  const showCostElement = document.querySelector(".js-cost");
+
+  if (cost < 40 && cost > 0) {
+    cost = (cost * 100 + 1000) / 100;
+    showCostElement.innerHTML = `$${cost}`;
+    showCostElement.classList.remove("cost-display");
+  } else if (cost < 0) {
+    showCostElement.innerHTML = `Error: cost cannot be less than $0`;
+    showCostElement.classList.add("cost-display");
   }
 }
